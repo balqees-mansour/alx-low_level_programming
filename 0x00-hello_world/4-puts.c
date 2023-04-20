60656bdadc10f53e7a140bab8299bf4e8f17c1f3
balqees-mansour <balqeesmansour0@gmail.com>
2023-04-20 13:08:18 -0700

    comit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Entry point 
+ * Description : A C program that prints with puts function
  *
- * Description : A C program that prints with put function 
- * Return: Always 0 success
+ * Return: Always 0 (success)
 */
 int main(void)
 {
-	puts("\" Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
