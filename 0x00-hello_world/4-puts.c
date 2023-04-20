16f42dc4a7a11326cf466f7ed1f41afb4c0032b7
balqees-mansour <balqeesmansour0@gmail.com>
2023-04-20 15:04:52 -0700

    edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
- * Description : A C program that prints with puts function
+ * main - Entry point
+ *
+ * Description: A C program that prints with puts function
  *
  * Return: Always 0 (success)
 */
