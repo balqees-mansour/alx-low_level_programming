d72091e5439fe89bde32623085463cbb0d6c76c2
balqees-mansour <balqeesmansour0@gmail.com>
2023-04-25 15:23:27 -0700

    betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * print_alphabet - print all alphabet in lowercase
 */
@@ -9,7 +8,8 @@ void print_alphabet(void)
 	char letter;
 
 
- 	for (letter = 'a'; letter <= 'z'; letter++)			_putchar(letter);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
 
 	_putchar('\n');
 }
